e1fa2bc2cb546cbb3cdb772bb35250c35f28c0ac
harshavardhancookiecolonel <143651669+harshavardhancookiecolonel@users.noreply.github.com>
2023-09-03 15:16:35 -0700

    Update Let's play a game.py

--- Stage2/Let's play a game.py
+++ Stage2/Let's play a game.py
@@ -1,1 +1,9 @@
+def Guess_the_word(word):
+    print("H A N G M A N")
+    word = str(input("Guess the word: "))
+    if word == "python":
+        print("You survived!")
+    else:
+        print("You lost!")
 
+Guess_the_word("python")
