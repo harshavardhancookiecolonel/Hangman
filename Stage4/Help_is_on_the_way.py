6b2da9d0b0807f32cb6dcd2c22f96e5530b52f11
harshavardhancookiecolonel <143651669+harshavardhancookiecolonel@users.noreply.github.com>
2023-09-04 12:37:24 -0700

    Update Help_is_on_the_way.py

--- Stage4/Help_is_on_the_way.py
+++ Stage4/Help_is_on_the_way.py
@@ -1,1 +1,18 @@
+import random
+
+def Guess_the_word():
+    print("H A N G M A N")
+    words = ('python', 'java', 'swift', 'javascript')
+    word = random.choice(words)
+    modified_word = word[:3] + '-' * (len(word) - 3)
+
+    print(f"Guess the word {modified_word}: ")
+    guess = str(input())
+
+    if guess.lower() == word:
+        print("You survived!")
+    else:
+        print("You lost!")
+
+Guess_the_word()
 
