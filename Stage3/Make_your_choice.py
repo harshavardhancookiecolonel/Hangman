5e0f181e87d4bb07f3c2d09cb27e3e379e19ff83
harshavardhancookiecolonel <143651669+harshavardhancookiecolonel@users.noreply.github.com>
2023-09-03 20:54:12 -0700

    Update Make_your_choice.py

--- Stage3/Make_your_choice.py
+++ Stage3/Make_your_choice.py
@@ -1,1 +1,12 @@
+import random
 
+def Guess_the_word(word):
+    print("H A N G M A N")
+    words = ('python', 'java', 'swift', 'javascript')
+    word = str(input("Guess the word: "))
+    if word == random.choice(words):
+        print("You survived!")
+    else:
+        print("You lost!")
+
+Guess_the_word("python")
